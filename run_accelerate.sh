4250a555c58b6be07c87b80003dcdbc878841fad
parshinsh <parshinshojaee@gmail.com>
2025-09-11 21:11:24 -0400

    grpo code release

--- run_accelerate.sh
+++ run_accelerate.sh
@@ -2,7 +2,7 @@
 
 # Start VLLM server with 2 GPUs
 echo "Starting VLLM server..."
-CUDA_VISIBLE_DEVICES=0,1,2 trl vllm-serve \
+CUDA_VISIBLE_DEVICES=0 trl vllm-serve \
   --model Qwen/Qwen2.5-0.5B-Instruct &
 
 # Wait a moment for server to start
@@ -10,7 +10,7 @@ sleep 50
 
 # Run GRPO training on remaining GPUs
 echo "Starting GRPO training for LLMSR..."
-CUDA_VISIBLE_DEVICES=0,1,2 accelerate launch \
+CUDA_VISIBLE_DEVICES=1,2,3 accelerate launch \
   --config_file accelerate_deepspeed_config.yaml \
   main.py --spec_path ./specs/specification_oscillator1_numpy.txt \
                --problem_name oscillator1 \
